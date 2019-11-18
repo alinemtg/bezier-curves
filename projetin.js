26c4384ef68262662c57e065d72405a13c0a4817
alinemtg <amtg@cin.ufpe.br>
2019-11-17 21:29:37 -0300

    some refactor, and now the content is controlled by checkboxes

--- projetin.js
+++ projetin.js
@@ -1,144 +1,87 @@
-// Projetinho de PG, II Unidade - 2019.2
-// Aline Gouveia - amtg 
 
-// `•.¸¸.•´´¯`••._.• ! ! ! ! •._.••`¯´´•.¸¸.•`
+/* Projetinho de PG, II Unidade  .
+   Aline Gouveia - amtg      · 　 ☆ · 　 　　
+                           ✧　 . · 　☆
+                        　   · 　☆ . ·
+                        　　 　　· ✧　.
+*/
+
+
+// `~·=·=·=·=·=·=·='☆ . · getting html elements · . ☆'=·=·=·=·=·=·=·~
 
 var canvas = document.getElementById('canvas')
+
+var avalNumb = document.getElementById('avalNumber')
+var b_createNewCurve = document.getElementById('createNewCurve')
+
+var b_prevCurve = document.getElementById('prevCurve')
+var b_nextCurve = document.getElementById('nextCurve')
+var b_deleteSelCurve = document.getElementById('deleteCurve')
+
+var c_showPolyg = document.getElementById('showPolyg')
+var c_showCurves = document.getElementById('showCurves')
+var c_showContrPoints = document.getElementById('showControlPoints') 
+
 var defaultMode = document.getElementById('defaultMode')
 
-if (canvas.getContext) {
-    var ctx = canvas.getContext('2d')
-    var points = []
 
-    canvas.addEventListener('click', function (){
+// ~·=·=·=·=·=·=·='☆ . · MAIN CODE · . ☆'=·=·=·=·=·=·=·~
 
-        var clickedPoint = getClickCoordinates(event, canvas)
-        
-        // `•.¸¸.•´´¯`••._.• - - - INSERT MODE - - - •._.••`¯´´•.¸¸.•`
+var curves = []
+var pointsPending = []
+var ctx = canvas.getContext('2d')
 
-        if (!defaultMode.checked){
-        
-        // `•.¸¸.•´´¯`••._.• when i click, a point is drawn •._.••`¯´´•.¸¸.•`        
+canvas.addEventListener('click', function(){
+
+    var clickedPoint = getClickCoordinates(event, canvas)
+
+    // `•.¸¸.•´´¯`••._.• when i click, a point is drawn •._.••`¯´´•.¸¸.•`        
         ctx.beginPath()
-        points.push (clickedPoint)
+        pointsPending.push (clickedPoint)
         ctx.arc (clickedPoint.x, clickedPoint.y, 4, 0,  Math.PI * 2)
-        ctx.fillStyle = "#ff0000"
+        ctx.fillStyle = "#7E8754"
         ctx.fill();
+
+})
+
+// ~·=·='☆ . · response to html elements · . ☆'=·=·~
+
+function createNewCurve(){
+    var curve = new Curve ()
+    curve.avalNumb = avalNumb.value
+    curve.controlPoints = pointsPending
+    curve.castejuju()
+    curves.push(curve)
+    pointsPending = []
+ 
+    c_showCurves.checked = true
+    console.log(c_showCurves.checked)
     
-        var pLen = points.length
-        
-        // `•.¸¸.•´´¯`••._.• there are lines between points! •._.••`¯´´•.¸¸.•`
-        if (pLen>1){
-            ctx.beginPath()
-            //ctx.setLineDash([2, 3]);
-            ctx.moveTo(points[pLen-2].x, points[pLen-2].y)
-            ctx.lineTo(clickedPoint.x, clickedPoint.y)
-            ctx.strokeStyle = "#FF0000"
-            ctx.lineWidth = 0.5
-            ctx.stroke()   
-        }
+    redrawInCanvas()
+}
 
-    }else{
-        
-        // `•.¸¸.•´´¯`••._.• - - - DELETE MODE - - - •._.••`¯´´•.¸¸.•`
-
-        console.log("lesgou excluir")
-        console.log(points)
-        
-        var index = -1
-        var pointToBeErased
-        
-        for (var i=0; i<points.length; i++){
-            var point = points [i]
-            if (Math.sqrt((clickedPoint.x-point.x)*(clickedPoint.x-point.x) + (clickedPoint.y-point.y)*(clickedPoint.y-point.y)) <= 4.3){
-                pointToBeErased = new Point (point.x, point.y) 
-                index = i
-                break
-            }
-        }
-
-        if (index > -1) {
-            clearHistory (ctx, points, pointToBeErased, index)
-            points.splice(index, 1)
-        }
-        index = -1
-
-    }
-
-    
-    console.log (" - - -")
-    if (points.length > 4){
-        var numbAvali = 500
-        var curvePoints = []
-        for (var t=0; t<=1; t+=1/numbAvali){
-            var Xaux = []
-            var Yaux = []
-            for (var i=0; i<=points.length-1; i++){
-                Xaux.push(points[i].x)
-                Yaux.push(points[i].y)
-            }
-            var curvePoint = new Point(castejuju(t, Xaux), castejuju(t, Yaux))
-            //console.log (curvePoint)
-            curvePoints.push(curvePoint)
-        }
-        
-        ctx.moveTo(curvePoints[0].x, curvePoints[0].y)
-        for (var i=1; i<curvePoints.length; i++){
-            ctx.lineTo(curvePoints[i].x, curvePoints[i].y)
-            ctx.strokeStyle = "#FFDEAD"
-            ctx.lineWidth = 0.5
-            ctx.stroke()   
+function redrawInCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(c_showCurves.checked)
+    if (c_showCurves.checked){
+        for (var c=0; c<curves.length; c++){
+            curves[c].showCurve(ctx)
         }
     }
-
-    })
-
-
-}else{ // get context failed
-    console.log("ih, deu ruim")
+    if (c_showPolyg.checked){
+        for (var c=0; c<curves.length; c++){
+            curves[c].showPolyg(ctx)
+           }
+    }
+    if (c_showContrPoints.checked){
+        for (var c=0; c<curves.length; c++){
+            curves[c].showContrPoints(ctx)
+        }
+    }
 }
 
 
-
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-// `•.¸¸.•´´¯`••._.• algorithm for bezier curve •._.••`¯´´•.¸¸.•`
-
-function castejuju (u, coord){
-    /* based on the pseudocode provided here: 
-    https://pages.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/Bezier/de-casteljau.html */ 
-    var n = coord.length;
-
-    for (var k=1; k <= n; k++){
-        for (var i=0; i < n-k; i++){
-            coord[i] = (1-u)*coord[i] + u*coord[i+1]
-            console.log(coord[i])
-       }
-    }
-    return coord[0];
-}
-
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-// `•.¸¸.•´´¯`••._.• some structures •._.••`¯´´•.¸¸.•`
-
-function Point(x, y) {
-    this.x = x
-    this.y = y
-
-    this.dot = function(numeroAe){
-        this.x = numeroAe*this.x
-        this.y = numeroAe*this.y
-    }
-   // this.sum = function(numeroAe){
-     //   this.x = numeroAe + x
-       // this.y = numeroAe + y
-   // }
-}
-
-// `•.¸¸.•´´¯`••._.• some methods •._.••`¯´´•.¸¸.•`
+// `~·=·=·=·=·=·=·='☆ . · some methods · . ☆'=·=·=·=·=·=·=·~
 
 function getClickCoordinates (event, canvas) {
         let rect = canvas.getBoundingClientRect()
@@ -148,41 +91,81 @@ function getClickCoordinates (event, canvas) {
         return (new Point(thisX, thisY))
 }
 
-function clearHistory (ctx, points, pointToBeErased, index){
-    
-    // erase point
-    ctx.beginPath()
-    ctx.arc (pointToBeErased.x, pointToBeErased.y, 4.8 , 0,  Math.PI * 2)
-    ctx.fillStyle = 'white'
-    ctx.strokeStyle = 'white'
-    ctx.fill()
 
-    // remove lines  ￼
-    if (index<points.length-1){
-        ctx.beginPath()
-        ctx.moveTo(pointToBeErased.x, pointToBeErased.y)
-        ctx.lineTo(points[index+1].x, points[index+1].y)
-        ctx.strokeStyle = 'white'
-        ctx.lineWidth = 3
-        ctx.stroke()
-    }
-    if (index>0){
-        ctx.beginPath()
-        ctx.moveTo(pointToBeErased.x, pointToBeErased.y)
-        ctx.lineTo(points[index-1].x, points[index-1].y)
-        ctx.strokeStyle = 'white'
-        ctx.lineWidth = 3
-        ctx.stroke()
-    }
-    
-    // draw line for remaing points
-    if (index<points.length-1 && index>0){
-        ctx.beginPath()
-        ctx.moveTo(points[index-1].x, points[index-1].y)
-        ctx.lineTo(points[index+1].x, points[index+1].y)
-        ctx.strokeStyle = "#FF0000"
-        ctx.lineWidth = 2
-        ctx.stroke()
+// ~·=·=·=·=·=·=·='☆ . · some structures · . ☆'=·=·=·=·=·=·=·~
 
+function Point(x, y) {
+    this.x = x
+    this.y = y
+}
+
+function Curve() {
+    this.avalNumb = 15,
+    this.controlPoints = [],
+    this.curvePoints = [],
+
+    // ~·=·='☆ . · algorithm for bezier curve · . ☆'=·=·~
+
+    this.castejuju  = function(){
+
+        console.log (avalNumb)
+        var n = this.controlPoints.length-1
+        var Qx = []
+        var Qy = []
+
+        for (var u=0; u<=1; u+=1/this.avalNumb){
+            for (var i=0; i<=n; i++){
+                Qx[i] = this.controlPoints[i].x
+                Qy[i] = this.controlPoints[i].y
+            }
+            for (var k=1; k<=n; k++){
+                for (var i=0; i<=n-k; i++){
+                    Qx[i] = (1-u)*Qx[i] + u*Qx[i+1]
+                    Qy[i] = (1-u)*Qy[i] + u*Qy[i+1]
+                }
+            }
+            var curvePoint = new Point (Qx[0], Qy[0])
+            this.curvePoints.push(curvePoint)
+        }
+    } 
+    ,
+
+    // ~·=·='☆ . · "show" methods · . ☆'=·=·~
+
+    this.showContrPoints = function(ctx){ 
+        for (var p=0; p<this.controlPoints.length; p++){
+           ctx.beginPath()
+           ctx.moveTo(this.controlPoints.x, this.controlPoints.y)
+           ctx.arc (this.controlPoints[p].x, this.controlPoints[p].y, 4, 0,  Math.PI * 2)
+           ctx.fillStyle = "#7E8754"
+           ctx.fill()
+        }
+    } 
+    ,
+    this.showPolyg = function(ctx){
+        ctx.beginPath()
+        var CPlen = this.controlPoints.length
+        if (CPlen>1){
+            ctx.moveTo(this.controlPoints[0].x, this.controlPoints[0].y)
+            for (var p=0; p<CPlen; p++){
+                ctx.lineTo(this.controlPoints[p].x, this.controlPoints[p].y)
+                ctx.strokeStyle = "#7E8754"
+                ctx.setLineDash([2, 3])
+                ctx.lineWidth = 0.5
+                ctx.stroke()  
+            }
+        }
+    }
+    ,
+    this.showCurve = function(ctx){
+        ctx.beginPath()
+        ctx.moveTo(this.curvePoints[0].x, this.curvePoints[0].y)
+        for (var i=0; i<this.curvePoints.length; i++){
+            ctx.lineTo(this.curvePoints[i].x, this.curvePoints[i].y)
+            ctx.strokeStyle = "#FFAAA4"
+            ctx.setLineDash([0]);
+            ctx.lineWidth = 0.7
+            ctx.stroke()
+        }
     }
 }
